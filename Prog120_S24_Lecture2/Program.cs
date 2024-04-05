4fe868ba2ba32a82e079841eabce6924f241f22e
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-04-04 19:12:20 -0700

    Update

--- Prog120_S24_Lecture2/Program.cs
+++ Prog120_S24_Lecture2/Program.cs
@@ -2,9 +2,87 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+            // Debugger
+
+            // Concatenate
+            // Adding two strings together to form one long string
+            string firstName = "Will";
+            string lastName = "Cram";
+            
+ 
+            Console.Write(firstName + " " + lastName);
+
+            string fullName = firstName + " " + lastName;
+            Console.WriteLine(fullName);
+
+            firstName = "Vitalli";
+            lastName = "Bobyr";
+            fullName = firstName + " " + lastName;
+
+            Console.WriteLine(fullName);
+
+            // Console.ReadLine()
+
+        } // End of Main
+
+        public static void StringExample()
+        {
+            // Variables
+            // type
+            // string, bool, int, double
+            // String - A collection of characters
+            // A string is defined with double quotes
+
+            // Declaring and initializing a variable
+            // equal sign, = , assignment operator
+            string teachersName = "Will";
+
+            // Declare a variable
+            // Naming convention for variables
+            string futureProgrammer;
+            string favoriteGame = "Tears of the Kingdom";
+
+            string x, q, y, XX, t, u;
+
+            // Declaring multiple variables of the same type in one line
+            string student1, student2, student3;
+
+            // Cannot use a variable before it's been declared
+            string bestWindInstrument;
+            bestWindInstrument = "Flute";
+
+
+            Console.Write("Hello, ");
+            Console.WriteLine(teachersName);
+
+            teachersName = "Hoang";
+
+            Console.Write("Hello, ");
+            Console.WriteLine(teachersName);
+        } // End of String Example
+
+        // public static void Name() { }
+        public static void Practice()
+        {
+            Console.WriteLine("Benny Goodman");
+            Console.WriteLine("--------------");
+            Console.WriteLine("Where and When");
+            Console.WriteLine("Sing, Sing, Sing");
+
+            // Console.WriteLine()
+            // Console.Write()
+
+            // Curly Brace
+
+            // Error
+            // Compile Time Error
+
+            // Comments
+        } // End of practice
+
+    } // end of class
+
+} // end of namespace
